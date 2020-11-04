3ef23f95647f74b4197336f68a4d5811e3d75b09
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2020-11-04 15:49:17 +0530

    Code for choosing options

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-
-options = [
-    "View drawings",
-    "Load from file",
-    "Generate random drawings"
-]
+options = ["View drawings", "Load from file", "Generate random drawings"]
 
 
 def load_option(op):
-    if op == 5:
+    if op == 1:
+        pass  # load the list of premeade drawings
+
+    elif op == 2:
+        pass  # ask the user for the file
+
+    elif op == 3:
+        pass  # Show the user a randomly generated drawing with option to save
+
+    else:
+        print("The option is not valid")
         start()
 
+
 def start():
-    
+
     print("Choose an option")
 
-    for index , op in enumerate(options):
+    for index, op in enumerate(options):
         print(f"{index + 1}) {op}")
 
     option = input()
     load_option(int(option))
-
 
 
 if __name__ == "__main__":
