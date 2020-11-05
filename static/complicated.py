dd0c85bd407dbe836e6a863bda118ada852b1bc5
Devadathan13 <69346006+Devadathan13@users.noreply.github.com>
2020-11-05 11:00:45 +0530

    Added simple.py and complicated.py to static

---
+++ static/complicated.py
@@ -0,0 +1,25 @@
+import turtle
+import random
+
+class Complicated():
+	def flowers(self):
+		bob=turtle.Turtle()
+		turtle.colormode(255) 
+		bob.speed(100)
+		for n in range(60):
+		    bob.penup()
+		    bob.goto(random.randint(-700, 700), random.randint(-700, 700))
+		    bob.pendown()
+		    red_amount   = random.randint( 1,255) #/ 100.0
+		    blue_amount  = random.randint(1, 255) #/ 100.0
+		    green_amount = random.randint( 1, 255)# / 100.0
+		    bob.pencolor((red_amount, green_amount, blue_amount))
+		    circle_size = random.randint(10, 40)
+		    bob.pensize(random.randint(1, 5))
+		    for i in range(6):
+		        bob.circle(circle_size)
+		        bob.left(60)
+		turtle.done()
+#--------------------------------------------------------------------------------------------------------------------
+'''a= Complicated()
+a.flowers()'''
---
+++ static/simple.py
@@ -0,0 +1,49 @@
+import turtle
+
+class Simple():
+	"""def __init__(self):
+		self.bob=turtle.Turtle()
+		self.bob.speed(10)"""
+#-----------------------------------------------------------------------------------------------------------
+	def wormhole(self):
+		ninja = turtle.Turtle()
+		ninja.speed(100)
+		for i in range(180):
+		    ninja.forward(200)
+		    ninja.right(30)
+		    ninja.forward(20)
+		    ninja.left(60)
+		    ninja.forward(50)
+		    ninja.right(30)
+		    ninja.penup()
+		    ninja.setposition(0, 0)
+		    ninja.pendown()
+		    ninja.right(2)
+		turtle.done()
+#------------------------------------------------------------------------------------------------------------
+	def color_spiral(self):
+		colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+		bob=turtle.Turtle()
+		bob.speed(50)
+		for x in range(360):
+		    bob.pencolor(colors[x % 6])
+		    bob.width(x / 100 + 1)
+		    bob.forward(x)
+		    bob.left(59)
+		turtle.done()
+#-------------------------------------------------------------------------------------------------------------
+	def protractor(self):
+		bob=turtle.Turtle()
+		for angle in range(0, 360, 15):
+		    bob.setheading(angle)
+		    bob.forward(250)
+		    bob.write(str(angle) + '°')
+		    bob.backward(250)
+		turtle.done()
+#-------------------------------------------------------------------------------------------------------------
+	def something(self):
+		pass
+
+#----------------------------------------------------------------------------------------------------------------
+'''a= Simple()# for testing purpose only
+a.protractor()'''
