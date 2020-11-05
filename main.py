3ed059ebcef426b2900a51e4e73871e07e339983
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2020-11-05 10:51:35 +0530

    Removed colour codes

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-from pathlib import Path
 from time import sleep
-
-
-# hold the headers for all the ANSI codes
-class Bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    RESET = '\u001b[0m'
-
 
 # All the options that the software provides.
 options = ["View drawings", "Load from file", "Generate random drawings"]
@@ -40,8 +24,7 @@ def start():
         print()
 
         for index, op in enumerate(options):
-            print(f"{index + 1}) {Bcolors.BOLD}{op}")
-            print(Bcolors.RESET)
+            print(f"{index + 1}) {op}")
 
         print()
         try:
@@ -60,20 +43,17 @@ def start():
                 break
 
         except ValueError:
-            print(f"{Bcolors.WARNING}Enter a valid option. \n")
-            print(Bcolors.RESET)
+            print(f"Enter a valid option. \n")
             sleep(1)
 
 
 if __name__ == "__main__":
-    print(f"{Bcolors.OKCYAN}Welcome to The Turtle Project")
-    print(Bcolors.RESET)
+    print(f"Welcome to The Turtle Project")
 
     try:
         print("Press Ctrl+C to exit")
         start()
 
     except KeyboardInterrupt:
-        print(f"{Bcolors.OKBLUE}Exiting, Thanks for using.")
+        print(f"Exiting, Thanks for using.")
         print()
-        print(Bcolors.RESET)
